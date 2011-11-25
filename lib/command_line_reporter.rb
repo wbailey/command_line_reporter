61cdb939b3d7e46585896dd3234fe6c6a9ef5e44
Wes Bailey <baywes@gmail.com>
2011-11-25 14:32:39 -0800

    introducing the idea of multiple formatters with progress and nested.  Should be a better way to access the formatter and declare their usage than what we have now

--- lib/command_line_reporter.rb
+++ lib/command_line_reporter.rb
@@ -1,28 +1,23 @@
+require 'nested_formatter'
+
 module CommandLineReporter
-  attr_accessor :clr_indent, :clr_complete_string, :clr_indent_size
+  attr_accessor :clr_formatter, :clr_indent, :clr_message_str, :clr_complete_string, :clr_indent_size
 
-  def report(options = {})
-    raise ArgumentError unless (options.keys - [:message, :type, :complete, :indent_size]).empty? && block_given?
-
-    self.clr_indent = (self.clr_indent) ? self.clr_indent += 1 : 0
-
-    padding = ' ' * self.clr_indent * (options[:indent_size] || self.clr_indent_size)
-
-    start_str = "#{padding}#{options[:message]}"
-    end_str = options[:complete] || self.clr_complete_string
-
-    if options[:type] == 'inline'
-      print "#{start_str}..."
-    else
-      puts start_str
-      end_str = padding + end_str
+  def report(options = {}, &block)
+    case self.clr_formatter
+    when 'progress'
+      clr_progress_formatter(block)
+    when 'nested'
+      clr_nested_formatter(options, block)
     end
+  end
 
-    yield
+  def clr_formatter
+    @clr_formatter ||= 'nested'
+  end
 
-    puts end_str
-
-    self.clr_indent -= 1
+  def clr_message_string
+    @clr_message_string ||= 'working'
   end
 
   def clr_complete_string
@@ -31,5 +26,31 @@ module CommandLineReporter
 
   def clr_indent_size
     @clr_indent_size ||= 2
+  end
+
+  private
+
+  def clr_nested_formatter(options, block)
+    raise ArgumentError unless (options.keys - [:message, :type, :complete, :indent_size]).empty? && !block.nil?
+
+    self.clr_indent = (self.clr_indent) ? self.clr_indent += 1 : 0
+
+    padding = ' ' * self.clr_indent * (options[:indent_size] || self.clr_indent_size)
+
+    message_str = padding + (options[:message] || self.clr_message_string)
+    complete_str = options[:complete] || self.clr_complete_string
+
+    if options[:type] == 'inline'
+      print "#{message_str}..."
+    else
+      puts message_str
+      complete_str = padding + complete_str
+    end
+
+    block.call
+
+    puts complete_str
+
+    self.clr_indent -= 1
   end
 end
--- spec/command_line_reporter_spec.rb
+++ spec/command_line_reporter_spec.rb
@@ -12,89 +12,214 @@ describe CommandLineReporter do
 
   describe 'default values' do
     its(:clr_indent) { should be_nil }
-    its(:clr_complete_string) { should eq 'complete' }
+    its(:clr_message_string) { should == 'working' }
+    its(:clr_complete_string) { should == 'complete' }
     its(:clr_indent_size) { should eq 2 }
   end
 
   describe "#report" do
-    context 'argument validation' do
-      it 'raises exception when there is an invalid argument' do
-        lambda {
-          subject.report(:asdf => true)
-        }.should raise_exception /ArgumentError/
+    context 'progress formatter' do
+      before :each do
+        subject.clr_formatter = 'progress'
       end
 
-      it 'raises an exception when a block is not given' do
-        lambda {
-          subject.report(:message => 'test')
-        }.should raise_exception /ArgumentError/
-      end
-
-      it 'accepts valid arguments' do
-        subject.should_receive(:puts).with('test')
-        subject.should_receive(:puts).with('complete')
-
-        lambda {
-          subject.report(:message => 'test') do
-          end
-        }.should_not raise_exception
+      it 'displays dots for progress' do
+        subject.should_receive(:print).exactly(10).times.with('.')
+        subject.report do
+          10.times {progress}
+        end
       end
     end
 
-    context 'not nested' do
-      it 'performs a wrapped report' do
-        subject.should_receive(:puts).with('test')
-        subject.should_receive(:puts).with('complete')
-        subject.report(:message => 'test') { }
+    context 'nested formatter' do
+      before :each do
+        subject.clr_formatter = 'nested'
       end
 
-      it 'performs an inline report' do
-        subject.should_receive(:print).with('test...')
-        subject.should_receive(:puts).with('complete')
-        subject.should_receive(:print).with('test2...')
-        subject.should_receive(:puts).with('complete')
-        subject.report(:message => 'test', :type => 'inline') { }
-        subject.report(:message => 'test2', :type => 'inline') { }
+      context 'argument validation' do
+        it 'raises exception when there is an invalid argument' do
+          lambda {
+            subject.report(:asdf => true)
+          }.should raise_exception /ArgumentError/
+        end
+
+        it 'raises an exception when a block is not given' do
+          lambda {
+            subject.report(:message => 'test')
+          }.should raise_exception /ArgumentError/
+        end
+
+        it 'accepts valid arguments' do
+          subject.should_receive(:puts).with('test')
+          subject.should_receive(:puts).with('complete')
+
+          lambda {
+            subject.report(:message => 'test') do
+            end
+          }.should_not raise_exception
+        end
       end
 
-      it 'overrides the default for all invocations of a wrapped report' do
-        subject.clr_complete_string = 'done'
-        subject.should_receive(:puts).with('test')
-        subject.should_receive(:puts).with('done')
-        subject.should_receive(:puts).with('test2')
-        subject.should_receive(:puts).with('done')
-        subject.report(:message => 'test') { }
-        subject.report(:message => 'test2') { }
+      context 'not nested' do
+        it 'performs a wrapped report' do
+          subject.should_receive(:puts).with('working')
+          subject.should_receive(:puts).with('complete')
 
+          subject.report() { }
+        end
+
+        it 'performs a wrapped report overriding the message' do
+          subject.should_receive(:puts).with('test')
+          subject.should_receive(:puts).with('complete')
+
+          subject.report(:message => 'test') { }
+        end
+
+        it 'performs an inline report' do
+          subject.should_receive(:print).with('test...')
+          subject.should_receive(:puts).with('complete')
+          subject.should_receive(:print).with('test2...')
+          subject.should_receive(:puts).with('complete')
+
+          subject.report(:message => 'test', :type => 'inline') { }
+          subject.report(:message => 'test2', :type => 'inline') { }
+        end
+
+        it 'overrides the default for all invocations of a wrapped report' do
+          subject.clr_complete_string = 'done'
+          subject.should_receive(:puts).with('test')
+          subject.should_receive(:puts).with('done')
+          subject.should_receive(:puts).with('test2')
+          subject.should_receive(:puts).with('done')
+
+          subject.report(:message => 'test') { }
+          subject.report(:message => 'test2') { }
+        end
+
+        it 'overrides the default complete string for a wrapped report' do
+          subject.should_receive(:puts).with('test')
+          subject.should_receive(:puts).with('done')
+          subject.should_receive(:puts).with('test2')
+          subject.should_receive(:puts).with('finally')
+
+          subject.report(:message => 'test', :complete => 'done') { }
+          subject.report(:message => 'test2', :complete => 'finally') { }
+
+          subject.clr_indent.should == -1
+        end
+
+        it 'overrides the default complete string for an inline report' do
+          subject.should_receive(:print).with('test...')
+          subject.should_receive(:puts).with('done')
+          subject.should_receive(:print).with('test2...')
+          subject.should_receive(:puts).with('finally')
+
+          subject.report(:message => 'test', :type => 'inline', :complete => 'done') { }
+          subject.report(:message => 'test2', :type => 'inline', :complete => 'finally') { }
+
+          subject.clr_indent.should == -1
+        end
+
+        it 'performs another wrapped report to ensure defaul behavior' do
+          subject.should_receive(:puts).with('test')
+          subject.should_receive(:puts).with('complete')
+          subject.should_receive(:puts).with('test2')
+          subject.should_receive(:puts).with('complete')
+
+          subject.report(:message => 'test') { }
+          subject.report(:message => 'test2') { }
+        end
       end
 
-      it 'overrides the default complete string for a wrapped report' do
-        subject.should_receive(:puts).with('test')
-        subject.should_receive(:puts).with('done')
-        subject.should_receive(:puts).with('test2')
-        subject.should_receive(:puts).with('finally')
-        subject.report(:message => 'test', :complete => 'done') { }
-        subject.report(:message => 'test2', :complete => 'finally') { }
-        subject.clr_indent.should == -1
-      end
+      context 'nested commands' do
+        it 'indents the nested wrapped messages' do
+          subject.should_receive(:puts).with('test')
+          subject.should_receive(:puts).with('  test2')
+          subject.should_receive(:puts).with('  complete')
+          subject.should_receive(:puts).with('complete')
 
-      it 'overrides the default complete string for an inline report' do
-        subject.should_receive(:print).with('test...')
-        subject.should_receive(:puts).with('done')
-        subject.should_receive(:print).with('test2...')
-        subject.should_receive(:puts).with('finally')
-        subject.report(:message => 'test', :type => 'inline', :complete => 'done') { }
-        subject.report(:message => 'test2', :type => 'inline', :complete => 'finally') { }
-        subject.clr_indent.should == -1
-      end
+          subject.report(:message => 'test') do
+            subject.report(:message => 'test2') { }
+          end
+        end
 
-      it 'performs another wrapped report to ensure defaul behavior' do
-        subject.should_receive(:puts).with('test')
-        subject.should_receive(:puts).with('complete')
-        subject.should_receive(:puts).with('test2')
-        subject.should_receive(:puts).with('complete')
-        subject.report(:message => 'test') { }
-        subject.report(:message => 'test2') { }
+        it 'indents the multiple nested wrapped messages' do
+          subject.should_receive(:puts).with('test')
+          subject.should_receive(:puts).with('  test2')
+          subject.should_receive(:puts).with('    test3')
+          subject.should_receive(:puts).with('    complete')
+          subject.should_receive(:puts).with('  complete')
+          subject.should_receive(:puts).with('complete')
+
+          subject.report(:message => 'test') do
+            subject.report(:message => 'test2') do
+              subject.report(:message => 'test3') do
+              end
+            end
+          end
+        end
+
+        it 'indents the nested wrapped and inline messages' do
+          subject.should_receive(:puts).with('test')
+          subject.should_receive(:print).with('  test2...')
+          subject.should_receive(:puts).with('complete')
+          subject.should_receive(:puts).with('complete')
+
+          subject.report(:message => 'test') do
+            subject.report(:message => 'test2', :type => 'inline') { }
+          end
+        end
+
+        it 'indents the multiple nested wrapped messages' do
+          subject.should_receive(:puts).with('test')
+          subject.should_receive(:puts).with('  test2')
+          subject.should_receive(:print).with('    test3...')
+          subject.should_receive(:puts).with('complete')
+          subject.should_receive(:puts).with('  complete')
+          subject.should_receive(:puts).with('complete')
+
+          subject.report(:message => 'test') do
+            subject.report(:message => 'test2') do
+              subject.report(:message => 'test3', :type => 'inline') do
+              end
+            end
+          end
+        end
+
+        it 'overrides the indent spacing of all messages' do
+          subject.should_receive(:puts).with('test')
+          subject.should_receive(:puts).with('    test2')
+          subject.should_receive(:print).with('        test3...')
+          subject.should_receive(:puts).with('complete')
+          subject.should_receive(:puts).with('    complete')
+          subject.should_receive(:puts).with('complete')
+
+          subject.clr_indent_size = 4
+
+          subject.report(:message => 'test') do
+            subject.report(:message => 'test2') do
+              subject.report(:message => 'test3', :type => 'inline') do
+              end
+            end
+          end
+        end
+        it 'overrides the indent spacing of specific message' do
+          subject.should_receive(:puts).with('test')
+          subject.should_receive(:puts).with('      test2')
+          subject.should_receive(:print).with('      test3...')
+          subject.should_receive(:puts).with('complete')
+          subject.should_receive(:puts).with('      complete')
+          subject.should_receive(:puts).with('complete')
+
+          subject.clr_indent_size = 4
+
+          subject.report(:message => 'test') do
+            subject.report(:message => 'test2', :indent_size => 6) do
+              subject.report(:message => 'test3', :type => 'inline', :indent_size => 3) do
+              end
+            end
+          end
+        end
       end
     end
   end
