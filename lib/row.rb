11cd61f3a81f6797e69ceba0a332de1aaa84c83e
Wes Bailey <baywes@gmail.com>
2011-12-29 00:25:44 -0800

    finalizing the row api

--- lib/row.rb
+++ lib/row.rb
@@ -1,3 +1,14 @@
-class Row
+require 'column'
+require 'forwardable'
 
+class Row
+  extend Forwardable
+
+  attr_accessor :columns
+
+  def initialize(columns = [])
+    self.columns = columns
+  end
+
+  def_delegator :@columns, :push, :add
 end
--- spec/row_spec.rb
+++ spec/row_spec.rb
@@ -2,11 +2,38 @@ require 'spec_helper'
 require 'row'
 
 describe Row do
+  before :each do
+    @cols = 10.times.map {|v| Column.new("test#{v}")}
+  end
+
   context 'Creation' do
-    it 'defaults options hash' do
+    it 'in the absence of columns' do
       expect {
         Row.new
       }.to_not raise_error
+    end
+
+    it 'accepts array of columns' do
+      expect {
+        Row.new(@cols)
+      }.to_not raise_error
+    end
+  end
+
+  context 'collection management' do
+    subject { Row.new }
+
+    it 'adds column to an empty row' do
+      subject.add(@cols[0])
+      subject.columns.size.should == 1
+      subject.columns[0].should == @cols[0]
+    end
+
+    it 'adds column to the end of row' do
+      row = Row.new(@cols)
+      coln = Column.new('test3')
+      row.add(coln)
+      row.columns.should == @cols << coln
     end
   end
 end
