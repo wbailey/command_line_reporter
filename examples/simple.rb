58ab5dbe9fe3bf9129eac64d3a06fdb298a01a1d
Wes Bailey <baywes@gmail.com>
2012-01-16 23:27:26 -0800

    example as demonstrated in the wiki

--- examples/simple.rb
+++ examples/simple.rb
@@ -1,34 +1,20 @@
 require 'command_line_reporter'
 
+include CommandLineReporter
+
 class Example
-  include CommandLineReporter
+  def initialize
+    self.formatter = 'progress'
+  end
 
   def run
-    x,y,z = 0,0,0
-
-    header(:title => 'Simple Report Example', :align => 'center', :timestamp => true, :rule => true)
-
-    report(:message => 'calculating sums') do
-      x = 2 + 2
-      y = 10 - x
-      z = x + y
-    end
-
-    footer(:title => 'Values', :width => 15)
-
-    table do
-      row do
-        column('x', :width => 5, :align => 'right')
-        column(x.to_s, :width => 10)
+    report do
+      sum = 0
+      10.times do
+        sum += 10
+        self.formatter.progress
       end
-      row do
-        column('y')
-        column(y.to_s)
-      end
-      row do
-        column('z')
-        column(z.to_s)
-      end
+      aligned("Sum: #{sum}")
     end
   end
 end
